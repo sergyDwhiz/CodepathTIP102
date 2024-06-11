fc38bcd0f124063a8c3408697e66a93f56acab41
Sergius-Nyah <sergiusnyah@gmail.com>
2024-06-11 14:27:01 +0100

    Fewer comments

--- Arrays/ContainsDuplicates/solution.py
+++ Arrays/ContainsDuplicates/solution.py
@@ -1,20 +1,12 @@
-# Contains Duplicates
-
 class Solution(object):
     def containsDuplicate(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        # Approach 1: Use a set to track seen elements
-        # Approach 2: Check if the length of the list is different from length
-        #               of the set of the list.
-        return len(set(nums)) != len(nums)
-
-        # Approach 1:
-        seen = set()
-        for i in nums:
-            for j in nums:
-                return True # Number found in set
-            seen.add(i)
-        return False
+        seen = set() # Initialize list of seen elements.
+        for i in nums: # Loop through elements in arr
+            if i in seen: # If duplicate exists, return true
+                return True
+            seen.add(i) # Else, add element to list.
+        return False # And return False (No duplicate)
