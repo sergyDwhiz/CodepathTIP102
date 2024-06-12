d72b16eb7946109cfefd9fa982766609e70c46a1
Sergius-Nyah <sergiusnyah@gmail.com>
2024-06-12 23:07:13 +0100

    Missing numbers

---
+++ Arrays/MissingNum/MissingN.py
@@ -0,0 +1,13 @@
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+# find the one that is missing from the array.
+
+# Example
+# Input: nums = [3, 0, 1]
+# Output: 2
+
+def missingNumber(nums):
+    sum1 = 0
+    sum = len(nums) * (len(nums) + 1) // 2 # sum of n numbers in the arr
+    for i in range(len(nums)):
+        sum1 += nums[i]  # sum of the numbers in the arr
+    return sum - sum1 # Subtract to get the missing number
--- Arrays/TwoSum/TwoSum.py
+++ Arrays/TwoSum/TwoSum.py
@@ -1,7 +1,13 @@
-# Return Indices of two numbers in an array that add up to a target element.
 class Solution(object):
-    def TwoSum(self, nums, target):
-        for i in range (len(nums)): # Loop through num and compare
-            for j in range (i+1, len(nums)): # Begin looping from the next num since its n>1
-                if nums[j] + nums[i]== target: # or target - nums[i] (Less optmized tho)
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+
+
+
+
+
