f6a301ce6c0600e9aebd9ccb4147377da4a1ba60
Sergius Nyah <sergiusnyah@gmail.com>
2024-09-19 02:37:47 +0100

    Add Medium problem

---
+++ Arrays/Product of Array Except Self/prodArray.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+	def productExceptSelf(self, nums: List[int]) -> List[int]:
+		length = len(nums)
+		answer = [1] * length
+
+		# Calculate left products
+		left_product = 1
+		for i in range(length):
+			answer[i] = left_product # Store left product in answer array
+			left_product *= nums[i] # Update left product with current elt and add to answer array
+
+		# Calculate right products and final result
+		right_product = 1
+		for i in range(length - 1, -1, -1): # Start from the end of the array
+			answer[i] *= right_product # Multiply left product with right product
+			right_product *= nums[i]
+
+		return answer
+
