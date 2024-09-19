87ed863c780826e64c71f8f343845e72b4fee231
Sergius Nyah <sergiusnyah@gmail.com>
2024-09-19 23:10:32 +0100

    Add LCS

--- Arrays/Longest Consecutive Sequence/LCS.py
+++ Arrays/Longest Consecutive Sequence/LCS.py
@@ -1,1 +1,14 @@
 # Gets the longest consecutive sequence in an array.
+def len_longest_sub(s:str) -> int:
+    my_set = set()
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)): # Loop through the string
+        while s[right] in my_set: #
+            my_set.remove(s[left])
+            left += 1
+        my_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
