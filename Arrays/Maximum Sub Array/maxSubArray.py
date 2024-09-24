aeaaac6b62c5381e0e7d106a439422fba4b9d13a
Sergius Nyah <sergiusnyah@gmail.com>
2024-09-24 15:15:18 +0100

    Either way

--- Arrays/Maximum Sub Array/maxSubArray.py
+++ Arrays/Maximum Sub Array/maxSubArray.py
@@ -14,3 +14,21 @@ class Solution:
                 max_global = max_current
 
         return max_global
+
+# OR
+def find_max_sum_sublist(nums):
+    if len(nums) < 1:
+        return 0
+
+    curr_max = nums[0]
+    global_max = nums[0]
+
+    for i in range(1, len(nums)):
+        if curr_max < 0:
+            curr_max = nums[i]
+        else:
+            curr_max += nums[i]
+        if global_max < curr_max:
+            global_max = curr_max
+
+    return global_max
