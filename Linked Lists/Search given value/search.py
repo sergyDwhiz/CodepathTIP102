6325d8875b8ddcfcbd5758317e3732bf08e53bdc
Sergius Nyah <sergiusnyah@gmail.com>
2024-10-02 13:56:36 +0100

    Add recursive approach

--- Linked Lists/Search given value/search.py
+++ Linked Lists/Search given value/search.py
@@ -11,3 +11,17 @@ def searchList(head, value):
             return True
         current = current.next
     return False
+
+
+## Recursive Approach
+def search(head, value):
+    current_node = head
+    return searchRecursive(current_node, value)
+
+def searchRecursive(node, value):
+    if(not node):
+        return False
+    if(node.data == value):
+        return True
+    # Recursively call next node in list
+    return searchRecursive(node.next, value)
