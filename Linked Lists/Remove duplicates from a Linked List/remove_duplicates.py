02d5b12ca456eef70d37858d7bf4f72d119c7cbb
Sergius Nyah <sergiusnyah@gmail.com>
2024-10-02 16:33:30 +0100

    Remove duplicates in a list

---
+++ Linked Lists/Remove duplicates from a Linked List/remove_duplicates.py
@@ -0,0 +1,14 @@
+# Definition for a linked list node
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    current = head
+    while current and current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
