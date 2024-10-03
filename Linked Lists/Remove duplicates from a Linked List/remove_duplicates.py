591a0ffcac6e0ddf75d0e77024e89776ebd5b87d
Sergius Nyah <sergiusnyah@gmail.com>
2024-10-03 10:59:50 +0100

    Use set to handle unsorted lists

--- Linked Lists/Remove duplicates from a Linked List/remove_duplicates.py
+++ Linked Lists/Remove duplicates from a Linked List/remove_duplicates.py
@@ -1,14 +1,19 @@
 # Definition for a linked list node
+
 class LinkedListNode:
     def __init__(self, data):
         self.data = data
         self.next = None
-
 def remove_duplicates(head):
+    seen = set()
     current = head
+    seen.add(current.data)
+    if not head:
+      return head
     while current and current.next:
-        if current.data == current.next.data:
+        if current.next.data in seen:
             current.next = current.next.next
         else:
-            current = current.next
+          seen.add(current.next.data)
+          current = current.next
     return head
