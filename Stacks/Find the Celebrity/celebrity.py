a3c4fba77f10349b270267fa8a2c4535f467f452
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-06 14:49:45 +0100

    Find celebrity

---
+++ Stacks/Find the Celebrity/celebrity.py
@@ -0,0 +1,21 @@
+def celebrity(MATRIX):
+    N = len(MATRIX)
+
+    # Find a potential candidate
+    candidate = 0
+    for i in range(1, N):
+
+        if MATRIX[candidate][i] == 1:
+
+            candidate = i
+
+    # verify potential candidate
+    # Candidate must ben known by everyone else
+    # Candidate myst not know anyone else
+
+    for i in range(N):
+        if i != candidate:
+            if MATRIX[candidate][i] == 1 or MATRIX[i][candidate] == 0: #
+                return -1
+
+    return candidate
