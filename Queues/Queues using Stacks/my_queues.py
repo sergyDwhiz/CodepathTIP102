a4ff63af43e99a8cf8563e4c52bdd732f67bacf5
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-06 09:05:18 +0100

    Queue with Stacks

---
+++ Queues/Queues using Stacks/my_queues.py
@@ -0,0 +1,34 @@
+from Stack import MyStack
+class NewQueue:
+    def __init__(self): #
+        self.main_stack = MyStack()
+        self.temp_stack = MyStack()
+
+
+    # Insert elements in the Queue
+    def enqueue(self, value):
+        # If main_stack and temp_stack is empty, push value into it.
+        if self.main_stack.is_empty() and self.temp_stack.is_empty():
+            self.main_stack.push(value)
+        else:
+            # if main stack is not empty, empty its contents into temp stack
+            while not self.main_stack.is_empty():
+                self.temp_stack.push(self.main_stack.pop())
+            # since main_stack is now empty, you can now push the values back to it.
+            self.main_stack.push(value)
+            # While temp_stack is not empty, pop its elements and move them to main stack (Main Queue operation)
+
+            while not self.temp_stack.is_empty():
+                self.main_stack.push(self.temp_stack.pop())
+
+    def dequeue(self):
+        # return a value from main_stack if not empty.
+        if self.main_stack.is_empty():
+            return None
+        else:
+            value = self.main_stack.pop()
+        return value
+
+    # Time complexity for enqueue operation is O(n), since it will take "n" time to enqueu "n" elements.
+    # Time complexity for dequeue is O(1), or constant time, since it will take thesame time to remove an element, despite its position in the queue.
+
