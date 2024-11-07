73c3a01c41c3f72bdf0bf18392e755e8550c60a0
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-07 03:44:33 +0100

    Use stacks

--- Stacks/Find the Celebrity/celebrity.py
+++ Stacks/Find the Celebrity/celebrity.py
@@ -4,18 +4,24 @@ def celebrity(MATRIX):
     # Find a potential candidate
     candidate = 0
     for i in range(1, N):
-
         if MATRIX[candidate][i] == 1:
-
             candidate = i
 
-    # verify potential candidate
-    # Candidate must ben known by everyone else
-    # Candidate myst not know anyone else
-
+    # Verify potential candidate using stacks
+    stack = []
     for i in range(N):
-        if i != candidate: # compare the candidate with every other element except itself
-            if MATRIX[candidate][i] == 1 or MATRIX[i][candidate] == 0: #
-                return -1
+        if i != candidate:
+            stack.append(i)
+
+    while len(stack) > 0:
+        person = stack.pop()
+
+        # If the potential candidate knows the person, or the person doesn't know the potential candidate,
+        # then the potential candidate is not a celebrity
+        if MATRIX[candidate][person] == 1 or MATRIX[person][candidate] == 0:
+            return -1
 
     return candidate
+
+# Time: O(n^2)
+# Space: O(n)
